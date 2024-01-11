ca227a6159f8827c56abb814b88ce8e3c187044a
Tristan F <LeoDog896@hotmail.com>
2024-01-11 07:36:35 -0500

    feat: base nfc reader/writer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,62 @@
 <script lang="ts">
+	let hasBeenClicked = false;
+	let data = "";
+	const decoder = new TextDecoder('utf-8');
+
 	async function onScan() {
+		if (hasBeenClicked) return;
+
+		hasBeenClicked = true;
 		console.log('User clicked scan button');
 
 		try {
 			const ndef = new NDEFReader();
 			await ndef.scan();
-			console.log('> Scan started');
 
 			ndef.addEventListener('readingerror', () => {
 				alert('Argh! Cannot read data from the NFC tag. Try another one?');
 			});
 
 			ndef.addEventListener('reading', (ev) => {
-				alert(`> Event: ${ev}`);
+				data = decoder.decode(ev.message.records[0].data);
 			});
 		} catch (error) {
 			alert('Argh! ' + error);
 		}
-	};
+	}
+
+	async function write() {
+		try {
+			const ndef = new NDEFReader();
+			await ndef.write('Hello world!');
+			alert('> Message written');
+		} catch (error) {
+			alert('Argh! ' + error);
+		}
+	}
 </script>
 
-<button on:click={onScan}>Scan</button>
+<main>
+	{#if !hasBeenClicked}
+		<button on:click={onScan}>Scan</button>
+	{:else}
+		<pre>{data}</pre>
+		<button on:click={write}>Write</button>
+	{/if}
+</main>
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		padding: 1rem;
+	}
+
+	button {
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
