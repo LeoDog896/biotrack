e2aadf2ce8afd5deb1c24d0493daa41a11a5334a
Tristan F <LeoDog896@hotmail.com>
2024-01-11 12:33:31 -0500

    feat(examples/pong): movement

--- src/routes/examples/pong/+page.svelte
+++ src/routes/examples/pong/+page.svelte
@@ -1,4 +1,4 @@
-<!-- <script lang="ts">
+<script lang="ts">
 	import { Canvas, Layer, type Render } from 'svelte-canvas';
 
     interface Player {
@@ -7,43 +7,62 @@
     }
 
     let player1: Player = {
-        y: 0,
+        y: 0.5,
         vy: 0,
     };
 
     let player2: Player = {
-        y: 0,
+        y: 0.5,
         vy: 0,
     };
 
     function keydown(event: KeyboardEvent) {
+        player1.vy = 0;
+        player2.vy = 0;
+
+        if (event.key === 'ArrowUp') {
+            player1.vy = -1;
+        } else if (event.key === 'ArrowDown') {
+            player1.vy = 1;
+        }
+        
+        if (event.key === 'w') {
+            player2.vy = -1;
+        } else if (event.key === 's') {
+            player2.vy = 1;
+        }
+    }
+
+    function keyup(event: KeyboardEvent) {
         switch (event.key) {
             case 'ArrowUp':
-                player1.vy = -1;
-                break;
             case 'ArrowDown':
-                player1.vy = 1;
+                player1.vy = 0;
                 break;
             case 'w':
-                player2.vy = -1;
-                break;
             case 's':
-                player2.vy = 1;
+                player2.vy = 0;
                 break;
         }
     }
 
-	const render: Render = ({ context, width, height }) => {
-        context.fillStyle = 'white';
+    const xPad = 50;
+    const padSize = 10;
+    const padHeight = 100;
 
-        // draw player 1
-        context.fillRect(0, player1.y, 10, 100);
-        // draw player 2
-        context.fillRect(width - 10, player2.y, 10, 100);
+	const render: Render = ({ context, width, height, time }) => {
+        context.fillStyle = 'white';
+        const min = padHeight / height / 2;
+
+        player1.y = Math.min(Math.max(player1.y + player1.vy / 40, min), 1 - min);
+        player2.y = Math.min(Math.max(player2.y + player2.vy / 40, min), 1 - min);
+
+        context.fillRect(xPad, player1.y * height - padHeight / 2, padSize, padHeight);
+        context.fillRect(width - padSize - xPad, player2.y * height - padHeight / 2, 10, padHeight);
 	};
 </script>
 
-<svelte:window on:keydown={keydown} />
+<svelte:window on:keydown={keydown} on:keyup={keyup} />
 
 <Canvas autoplay>
 	<Layer {render} />
@@ -52,5 +71,6 @@
 <style>
     :global(html, body) {
         background-color: black;
+        overflow-y: hidden;
     }
-</style> -->
+</style>
