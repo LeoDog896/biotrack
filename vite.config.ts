03abf13779fa8bdab8c884d24859358caf858223
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-10 20:44:54 -0500

    chore: add @vitejs/plugin-basic-ssl
    fixes https errors locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), basicSsl()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
