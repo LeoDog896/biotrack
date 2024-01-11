b6a5f0ebe5a012f20d0c0b7b2566334aabecf712
Tristan F <LeoDog896@hotmail.com>
2024-01-11 07:45:07 -0500

    feat: move player page to /player

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<slot />
+
+<style global>
+    :global(html, body) {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    ::global(*) {
+        box-sizing: border-box;
+    }
+</style>
