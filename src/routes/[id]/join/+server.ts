c25e48dbd664a3a17125b724d1400cb6d4d11f4c
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-10 20:01:04 -0500

    feat: make [id]/join to work

--- src/routes/[id]/join/+server.ts
+++ src/routes/[id]/join/+server.ts
@@ -1,5 +1,39 @@
 import { json } from '@sveltejs/kit';
+import { prisma } from '$lib/prismaConnection.js'
 
-export const POST = ({ params }) => {
-	return json(params);
+export const POST = async ({ params, url }) => {
+	const userId = url.searchParams.get('user');
+
+	if (!userId) {
+		return json({ error: 'User ID not specified' });
+	}
+
+	const game = await prisma.game.findUnique({
+		where: {
+			id: parseInt(params.id)
+		}
+	});
+
+	if (!game) {
+		return json({ error: 'Game not found' });
+	}
+
+	const user = await prisma.user.findUnique({
+		where: {
+			id: userId
+		}
+	});
+
+	if (!user) {
+		return json({ error: 'User not found' });
+	}
+
+	const joinRequest = await prisma.joinRequest.create({
+		data: {
+			gameId: game.id,
+			userId: user.id
+		}
+	})
+
+	return json(joinRequest);
 };
