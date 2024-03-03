833ae5a46cdadd4e9ea5841217a76a5712685769
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-03-03 15:51:12 -0500

    feat: better home page scannable

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	import AndroidLogo from './android.svg?url';
+	import ChromeLogo from './chrome.svg?url';
+
 	import QR from '@svelte-put/qr/img/QR.svelte';
 
 	export let data;
@@ -10,6 +13,12 @@
 	{#if data.localAddress}
 		<div class="qr-container">
 			<div class="qr">
+				<h1>Scan for Player Management</h1>
+				<h2>
+					Requires
+					<img src={AndroidLogo} alt="Android Logo" width="20" height="20" />
+					<img src={ChromeLogo} alt="Chrome Logo" width="20" height="20" />
+				</h2>
 				<QR
 					data="https://{data.localAddress}:5173/player"
 					anchorOuterFill="#0954b8"
@@ -25,13 +34,23 @@
 	<title>Dashboard</title>
 </svelte:head>
 
-<style>
+<style lang="scss">
 	main {
 		margin: 1rem;
 	}
 
 	.qr {
 		width: 30%;
+		padding: 1rem;
+		background-color: rgba(0, 0, 0, 0.1);
+
+		h1 {
+			text-align: center;
+		}
+
+		h2 {
+			text-align: center;
+		}
 	}
 
 	.qr-container {
