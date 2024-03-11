5f8f65752f31b0f148e2b902eff4b2e5c229eb58
Tristan F <LeoDog896@hotmail.com>
2024-03-11 13:59:31 -0400

    feat: allow player searching

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+    import Fuse from 'fuse.js';
+
     export let data;
+
+    const fuse = new Fuse(data.users, {
+        keys: ['name']
+    });
+
+    let input = '';
+
+    $: filteredUsers = input ? fuse.search(input).map(user => user.item) : data.users;
 </script>
 
 <main>
     <h1>Players</h1>
 
-    {#each data.users as user}
+    <input type="text" placeholder="Search for a player" bind:value={input} />
+
+    {#each filteredUsers as user}
         <a href={`/player/${user.id}`}>
             <h2>{user.name}</h2>
         </a>
