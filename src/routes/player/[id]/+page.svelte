c4cb6614fac04977223a485e750ea2bff85cd2ac
Tristan F <LeoDog896@hotmail.com>
2024-03-11 16:00:06 -0400

    feat: editable name

--- src/routes/player/[id]/+page.svelte
+++ src/routes/player/[id]/+page.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
+
 	export let data;
+
+    let nameInput = data.user.name;
+    const assignNameInput = (x: string) => nameInput = x
+    $: assignNameInput(data.user.name);
+    let nameSubmissionButton: HTMLButtonElement;
 </script>
 
 <main>
-	<h1>Player <span class="gray">//</span> {data.user.name}</h1>
+	<h1>
+        Player
+        <span class="gray">//</span>
+        <form use:enhance action="/player/{data.user.id}?/name" method="POST">
+            <input type="text" name="name" value={nameInput} on:change={() => nameSubmissionButton.click()} />
+            <button type="submit" hidden bind:this={nameSubmissionButton}></button>
+        </form>
+    </h1>
 	<p>cuid: {data.user.id}</p>
 	<p>created at: {data.user.createdAt}</p>
 	{#if data.user.updatedAt.toString() !== data.user.createdAt.toString()}
@@ -18,7 +32,23 @@
 
 	h1 {
 		text-align: center;
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+        align-items: center;
 	}
+
+    form {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+    }
+
+    input {
+        padding: 0.5rem;
+        font-size: 1rem;
+        text-align: center;
+    }
 
 	.gray {
 		opacity: 0.5;
