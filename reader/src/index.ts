cc217a83ec98d0df2f9ddf8edca2b212465b0338
Tristan F <LeoDog896@hotmail.com>
2024-03-13 13:19:25 -0400

    feat: init some sort of trpc server

--- reader/src/index.ts
+++ reader/src/index.ts
@@ -1,1 +1,36 @@
-console.log(1)
+import { initTRPC } from '@trpc/server';
+import {
+  createHTTPServer,
+} from '@trpc/server/adapters/standalone';
+import { z } from 'zod';
+
+const t = initTRPC.create();
+
+const appRouter = t.router({
+    read: t.procedure
+      .query(() => {
+        // TODO: query for card
+        return 'data';
+      }),
+    write:
+      t.procedure
+        .input(
+            z.object({
+                data: z.string()
+            })
+        )
+        .mutation(() => {
+            // TODO: write to card
+        })
+});
+
+// Export the app router type to be imported on the client side
+export type AppRouter = typeof appRouter;
+
+// Create HTTP server
+const { listen } = createHTTPServer({
+  router: appRouter,
+});
+
+// Listen on port 2022
+listen(2022);
