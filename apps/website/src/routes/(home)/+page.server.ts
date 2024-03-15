e537397754a8997e6b21b7e91e60ce9dba880a11
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-03-15 15:47:38 -0400

    refactor: calculate local address as a function

--- apps/website/src/routes/(home)/+page.server.ts
+++ apps/website/src/routes/(home)/+page.server.ts
@@ -2,22 +2,22 @@ import { networkInterfaces } from 'node:os';
 
 const interfaces = networkInterfaces();
 
-let localAddress: string | undefined;
+function calculateLocalAddress(): string | undefined {
+	for (const [key, networks] of Object.entries(interfaces)) {
+		if (!networks) continue;
+		for (const { address } of networks) {
+			if (key === 'wlo1' && address.includes('.')) {
+				return address;
+			}
 
-for (const [key, networks] of Object.entries(interfaces)) {
-	if (!networks) continue;
-	for (const { address } of networks) {
-		if (key === 'wlo1' && address.includes('.')) {
-			localAddress = address;
-			break;
-		}
-
-		if ((key === 'Wi-Fi' && address.includes('.')) || address.startsWith('192.168')) {
-			localAddress = address;
-			break;
+			if ((key === 'Wi-Fi' && address.includes('.')) || address.startsWith('192.168')) {
+				return address;
+			}
 		}
 	}
 }
+
+const localAddress = calculateLocalAddress();
 
 if (!localAddress) {
 	throw new Error('No local address found');
